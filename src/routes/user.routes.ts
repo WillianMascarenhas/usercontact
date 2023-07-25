076b8ddd58617a87c6daea95fcd57fc4edd86dac
WillianMascarenhas <masca.will@gmail.com>
2023-07-25 20:56:30 -0300

    feat: rota de login/ criação do token

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,7 +2,9 @@ import { Router } from "express";
 import { createUserController } from "../controllers/users.controller";
 import { ensureDataIsValid } from "../middlewares/ensureDataIsValid.middleware";
 import { userSchemasRequest } from "../schemas/users.schemas";
+import { loginController } from "../controllers/login.controller";
 
 export const userRoutes = Router();
 
 userRoutes.post("/users", ensureDataIsValid(userSchemasRequest), createUserController);
+userRoutes.post("/login", loginController);
