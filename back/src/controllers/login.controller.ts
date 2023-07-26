692866cd8703a7c2a796ee9315321899c402fd0a
WillianMascarenhas <masca.will@gmail.com>
2023-07-25 23:24:02 -0300

    feat: descrição na controller de login

--- back/src/controllers/login.controller.ts
+++ back/src/controllers/login.controller.ts
@@ -2,7 +2,8 @@ import { Response, Request } from "express";
 import { createTokenService } from "../services/login/createToken.service";
 
 export const loginController = async (req: Request , res: Response) =>{
+    // Fazer login e criar o token
     const createToken = await createTokenService(req.body)
 
     return res.json({"token":createToken})
-}
+}   
