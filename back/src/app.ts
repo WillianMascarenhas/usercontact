c5912bcb738a7316575be8f08adfa944fd66492e
WillianMascarenhas <masca.will@gmail.com>
2023-07-27 17:02:58 -0300

    feat: cors para ligação do front com back

--- back/src/app.ts
+++ back/src/app.ts
@@ -5,10 +5,16 @@ import { userRoutes } from "./routes/user.routes";
 import { handlerAppError } from "./middlewares/handlerAppError.middleware";
 import { contactRoutes } from "./routes/contact.routes";
 import { loginRouter } from "./routes/login.routes";
+import cors from "cors"
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+    origin: "http://localhost:5173"
+}))
+// se alguma rota estiver acima dessa chamada, o cors não será aplicado para ela
+// essa url passada acima e a mesma da do front 
 app.use("/user", userRoutes);
 app.use("/contact", contactRoutes);
 app.use("/login", loginRouter)
